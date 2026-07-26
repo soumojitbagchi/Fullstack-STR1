977fe24e4b3f6978e37902b1b88ff0efb14f1201
soumojitbagchi <soumojitbagchi001@gmail.com>
2026-07-26 22:28:47 +0530

    Integrate Socket.IO for real-time sync

--- Backend/Server.js
+++ Backend/Server.js
@@ -1,8 +1,23 @@
 require("dotenv").config();
 const app = require("./src/app");
+const {createServer} = require('http')
+const {Server} = require('socket.io')
 const port =8080
 const connectToDB = require("./src/config/database");
 connectToDB();
-app.listen(port, () => {
-  console.log("connect to 8080"); 
-});
+const httpServer = createServer(app)
+const io = new Server(httpServer,()=>{
+  
+})
+io.on('connection',(socket)=>{
+  console.log('new connection established')
+  socket.on('sync',(msg)=>{
+    console.log(msg)
+  })
+})
+// httpServer.listen(port, () => {
+//   console.log("connect to 8080"); 
+// });
+app.listen(port,()=>{
+  console.log("connect to the port")
+})
