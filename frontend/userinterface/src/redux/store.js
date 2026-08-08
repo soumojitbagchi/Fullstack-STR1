c3bdbbbad9f8dba8a76b11a757799e69bc1aaf97
soumojitbagchi <soumojitbagchi001@gmail.com>
2026-08-08 07:39:57 +0530

    add user slice for authentication management

--- frontend/userinterface/src/redux/store.js
+++ frontend/userinterface/src/redux/store.js
@@ -1,9 +1,8 @@
-import {configureStore} from "@reduxjs/toolkit";
+import { configureStore } from "@reduxjs/toolkit";
 import counterReducer from "./counterSlice";
 
 export const store = configureStore({
   reducer: {
     counter: counterReducer,
-  
-},
+  },
 });
